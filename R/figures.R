955da1091ea53c58e0ab56b8a05e1a00f8b97aae
Andrew Heiss <andrewheiss@gmail.com>
2013-12-04 22:13:46 -0500

    Make figure print-ready

--- R/figures.R
+++ R/figures.R
@@ -12,10 +12,8 @@ generate.plot.data <- function(x) {
   # Create other table values
   dv <- gsub('_pct_fulfilled', '', x)
   
-  df$variation <- factor(df$variation, levels=c(0, 1, 3, 5), labels=c('Baseline', 'Collaboration with cost', 'Costless collaboration', 'Market'))
-  df$motivation <- factor(df$community_motivation, levels=c(0, 1), labels=c('Individual', 'Community'))
-  
-  #   community.type <- ifelse()
+  df$variation <- factor(df$variation, levels=c(0, 5, 3, 1), labels=c('Baseline', 'Market', 'Costless collaboration', 'Collaboration with cost'), ordered=TRUE)
+  df$motivation <- factor(df$community_motivation, levels=c(1, 0), labels=c('Community', 'Individual'))
   
   res.freq.name <- paste(substr(x, 1, 2), '_high_freq', sep='')
   res.freq <- simulation[1, res.freq.name]
@@ -28,8 +26,8 @@ generate.plot.data <- function(x) {
   objective.value <- ifelse(substr(x, 2, 2) == 1, 'High', 'Low')
   resource.name <- substr(x, 1, 1)
   
-  #   pretty.label <- paste(dv, '\n', resource.prevalence, ' (res.)\n', objective.prevalence, ' (obj.)', sep='')
-  pretty.label <- paste(dv, '\n', resource.prevalence, '\n', objective.prevalence, sep='')
+  pretty.label <- dv
+  # pretty.label <- paste(dv, '\n', resource.prevalence, '\n', objective.prevalence, sep='')
   
   return(list('motivation'=df$motivation, 'variation'=df$variation, 'resource_prevalence'=resource.prevalence, 'objective_prevalence'=objective.prevalence, 'objective_value'=objective.value, 'dv'=dv, 'pretty.label'=pretty.label, 'resource'=resource.name, 'pct_fulfilled'=df$d..x..))
 }
@@ -42,16 +40,18 @@ too.perfect <- which(plot.data$pct_fulfilled == 1)
 noise <- rnorm(length(too.perfect))/100000
 plot.data[too.perfect,'pct_fulfilled'] <- plot.data[too.perfect,'pct_fulfilled'] - noise
 
-# p <- ggplot(data=subset(plot.data, variation!='Baseline' & motivation=='Individual'), aes(x=pretty.label, y=pct_fulfilled, fill=resource))
-# p <- ggplot(data=subset(plot.data, variation!='Baseline' & motivation=='Community'), aes(x=pretty.label, y=pct_fulfilled, fill=resource))
+# Plot this stuff
 p <- ggplot(data=plot.data, aes(x=pretty.label, y=pct_fulfilled, fill=resource))
-# Violin plot
-violin <- p + geom_violin(scale="width") + 
-  stat_summary(aes(group=1), fun.y=mean, geom="point", size=5) + 
-  scale_y_continuous(labels=percent) + labs(x=NULL, y=NULL) + 
-  facet_wrap(~motivation+variation, nrow=2) + guides(fill=FALSE) + theme_bw()
 
-ggsave(plot=violin, filename="../Output/figure_1.pdf", width=7, height=5, scale=2)
+# Violin plot
+violin <- p + geom_violin(scale="width", size=0.25) + 
+  stat_summary(aes(group=1), fun.y=mean, geom="point", size=2) + 
+  scale_y_continuous(labels=percent) + labs(x=NULL, y=NULL) + 
+  facet_wrap(~motivation+variation, nrow=2) + guides(fill=FALSE) + 
+  scale_fill_grey(start=0.2, end=0.95) + theme_bw() + 
+  theme(axis.text=element_text(size=6), strip.text=element_text(size=6, face = 'bold'))
+
+ggsave(plot=violin, filename="../Output/figure_1.pdf", width=7, height=5, units="in")
 
 # Bar plot with error bars
 # p + stat_summary(aes(group=1), fun.y=mean, geom="bar") + 
