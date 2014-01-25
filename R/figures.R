be3feca4439027b33de2ddf53391dcbc2e388ff1
Andrew Heiss <andrewheiss@gmail.com>
2014-01-24 21:10:25 -0500

    Added distinguishing aesthetics and legend

--- R/figures.R
+++ R/figures.R
@@ -1,6 +1,7 @@
 # Load data and libraries
 source("load_data.R")
 library(ggplot2)
+library(grid)
 library(scales)
 library(reshape2)
 library(plyr)
@@ -13,11 +14,11 @@ generate.plot.data <- function(x) {
   dv <- gsub('_pct_fulfilled', '', x)
   
   df$variation <- factor(df$variation, levels=c(0, 5, 3, 1), labels=c('Baseline', 'Market', 'Costless collaboration', 'Collaboration with cost'), ordered=TRUE)
-  df$motivation <- factor(df$community_motivation, levels=c(1, 0), labels=c('Community', 'Individual'))
+  df$motivation <- factor(df$community_motivation, levels=c(1, 0), labels=c('Social', 'Individual'))
   
   res.freq.name <- paste(substr(x, 1, 2), '_high_freq', sep='')
   res.freq <- simulation[1, res.freq.name]
-  resource.prevalence <- ifelse(res.freq == 1, 'High', 'Low')
+  resource.prevalence <- ifelse(res.freq == 1, 'High (left)', 'Low (right)')
   
   obj.count.name <- paste(substr(x, 1, 2), '_count', sep='')
   obj.count <- simulation[1, obj.count.name]
@@ -41,26 +42,22 @@ noise <- rnorm(length(too.perfect))/100000
 plot.data[too.perfect,'pct_fulfilled'] <- plot.data[too.perfect,'pct_fulfilled'] - noise
 
 # Plot this stuff
-p <- ggplot(data=plot.data, aes(x=pretty.label, y=pct_fulfilled, fill=resource))
-
-# Violin plot
-violin <- p + geom_violin(scale="width", size=0.25) + 
+# aes(alpha=...) is included as a dummy legend aesthetic so resource prevalence can be included. Transparency is removed below.
+p <- ggplot(data=plot.data, aes(x=pretty.label, y=pct_fulfilled, fill=objective_prevalence, alpha=resource_prevalence))
+violin <- p + geom_violin(scale="width", size=0.25, aes(linetype=objective_value)) +
   stat_summary(aes(group=1), fun.y=mean, geom="point", size=2) + 
+  geom_vline(xintercept=4.5, colour="darkgrey", linetype=2) + 
   scale_y_continuous(labels=percent) + labs(x=NULL, y=NULL) + 
-  facet_wrap(~motivation+variation, nrow=2) + guides(fill=FALSE) + 
-  scale_fill_grey(start=0.2, end=0.95) + theme_bw() + 
-  theme(axis.text=element_text(size=6), strip.text=element_text(size=6, face = 'bold'))
+  facet_wrap(~ motivation + variation, nrow=2) + 
+  scale_fill_grey(start=0.4, end=0.8, name="Objective prevalence") + 
+  scale_alpha_discrete(range = c(1, 1), name="Resource prevalence") + # Remove transparency
+  scale_linetype(name="Objective value") + 
+  guides(fill=guide_legend(override.aes=list(size=0, linetype=0), order=2),
+         alpha=guide_legend(override.aes=list(size=0, linetype=0), order=1, keywidth=unit(0, "line"))) + 
+  theme_bw() + theme(legend.key = element_blank()) + 
+  theme(axis.text=element_text(size=6), strip.text=element_text(size=6, face='bold'), 
+        legend.text=element_text(size=6), legend.title=element_text(size=6),
+        legend.key.size=unit(.7,"line"),
+        legend.position="bottom", legend.box="horizontal")
 
 ggsave(plot=violin, filename="../Output/figure_1.pdf", width=7, height=5, units="in")
-
-# Bar plot with error bars
-# p + stat_summary(aes(group=1), fun.y=mean, geom="bar") + 
-#   scale_y_continuous(labels=percent) + labs(x=NULL, y=NULL) + 
-#   stat_summary(fun.data = mean_sdl, geom = "errorbar", mult = 1) + 
-#   facet_wrap(~motivation+variation, nrow=2) + guides(fill=FALSE) + theme_bw()
-
-# Bar plot with jittered points
-# p + stat_summary(aes(group=1), fun.y=mean, geom="bar") + 
-#   geom_point(position="jitter", alpha=0.1) + 
-#   scale_y_continuous(labels=percent) + labs(x=NULL, y=NULL) + 
-#   facet_wrap(~motivation+variation, nrow=2) + guides(fill=FALSE) + theme_bw()
